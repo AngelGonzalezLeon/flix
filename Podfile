56a748d1f5f6f255d3fc0cf1f60d0efac725b72a
angel gonzalez <angelgonzalez@mac.local>
2018-02-04 21:07:32 -0800

    Fixed reload error, and added animation for loading

--- Podfile
+++ Podfile
@@ -7,6 +7,7 @@ target 'flix' do
 
   # Pods for flix
 	pod 'AlamofireImage', '~> 3.1'
+	pod 'KRProgressHUD'
   target 'flixTests' do
     inherit! :search_paths
     # Pods for testing
--- flix/NowPlayingViewController.swift
+++ flix/NowPlayingViewController.swift
@@ -8,27 +8,42 @@
 
 import UIKit
 import AlamofireImage
+import KRProgressHUD
+
 class NowPlayingViewController: UIViewController,UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     var movies: [[String: Any]] = []
     var refreshControl: UIRefreshControl!
     
+    
+    
     override func viewDidLoad() {
+        // Start the activity indicator
+        KRProgressHUD.show()
         super.viewDidLoad()
-        let refreshControl = UIRefreshControl()
+        
+        
+        refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
         
         fetchMovies()
+        
+        // Stop the activity indicator
+        // Hides automatically if "Hides When Stopped" is enabled
+        DispatchQueue.main.asyncAfter(deadline: .now()+1) {
+            KRProgressHUD.dismiss()
+        }
     }
     @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
-        fetchMovies()
         
-    }
+        fetchMovies()
+        }
     func fetchMovies()
     {
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate:nil, delegateQueue: OperationQueue.main)
@@ -44,6 +59,7 @@ class NowPlayingViewController: UIViewController,UITableViewDataSource {
                 self.refreshControl.endRefreshing()
             }
         }
+        
         task.resume()
     }
     
