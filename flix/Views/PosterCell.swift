8c8e7efb5a995db056595bbb887796a7b19bb123
angel gonzalez <angelgonzalez@Mac.local>
2018-02-11 21:26:55 -0800

    Finished week 2 implementations

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by angel gonzalez on 2/11/18.
+//  Copyright © 2018 angel gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
